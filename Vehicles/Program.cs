2dea822872fcaf9a157cb42df625771a668f39cc
Rolf Petersen <rollep7@msn.com>
2016-11-08 10:27:00 +0100

    Add project files.

---
+++ Vehicles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine("Journey by airplane:");
+            Airplane myPlane = new Airplane();
+            myPlane.StartEngine("Contakt");
+            myPlane.TakeOff();
+            myPlane.Drive();
+            myPlane.Land();
+            myPlane.StartEngine("Whirr");
+
+            Console.WriteLine("\nJourney by car:");
+            Car myCar = new Car();
+            myCar.StartEngine("Brm brm");
+            myCar.Accelerate();
+            myCar.Drive();
+            myCar.Brake();
+            myCar.StartEngine("Phut phut");
+
+            Console.WriteLine("\nTesting polymorphism");
+            Vehicle v = myCar;
+            v.Drive();
+            v = myPlane;
+            v.Drive();
+        }
+        
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
